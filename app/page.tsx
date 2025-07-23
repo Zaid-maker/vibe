987c04a6c82f0fce3e46e64a5e90a818347e26d1
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-07-23 17:13:19 +0500

    testing complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 
-const Page = () => {
+const Page = async () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold">Welcome to the Home</h1>
