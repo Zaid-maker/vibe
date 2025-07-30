91c818affb35c15ca0d980fadf28c8a1c68fa0ec
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-07-30 15:03:37 +0500

    Remove unused TRPC client import and commented-out code from server.tsx

--- trpc/server.tsx
+++ trpc/server.tsx
@@ -5,7 +5,6 @@ import { cache } from 'react';
 import { createTRPCContext } from './init';
 import { makeQueryClient } from './query-client';
 import { appRouter } from './routers/_app';
-import { createTRPCClient, httpLink } from '@trpc/client';
 
 // IMPORTANT: Create a stable getter for the query client that
 //            will return the same client during the same request.
@@ -14,13 +13,5 @@ export const getQueryClient = cache(makeQueryClient);
 export const trpc = createTRPCOptionsProxy({
     ctx: createTRPCContext,
     router: appRouter,
-    queryClient: getQueryClient,
-});
-
-// If your router is on a separate server, pass a client:
-createTRPCOptionsProxy({
-    client: createTRPCClient({
-        links: [httpLink({ url: '...' })],
-    }),
     queryClient: getQueryClient,
 });
